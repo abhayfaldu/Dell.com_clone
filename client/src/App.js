81f4b0976e251fb2f360698bbb4f186e40b149af
Deepak Kumar Fathkar <deepak.fathkar@gmail.com>
2023-02-22 22:19:03 +0530

    reset page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
 import AllRoutes from "./MainRoutes/AllRoutes";
-import Reset from "./Pages/Reset";
-// import Login from "./Pages/Login"
 
 function App() {
   return (
     <div className="App">
       <AllRoutes />
-      {/* <Login /> */}
-      <Reset />
     </div>
   );
 }
