3b041bec8c3fde688d63e5316256ece1e4df7cf7
abhayfaldu <abhayfaldu1922@gmail.com>
2023-02-26 12:47:46 +0530

    added link to singleProduct page

--- client/src/Components/Product_list/ProductCard.jsx
+++ client/src/Components/Product_list/ProductCard.jsx
@@ -4,9 +4,11 @@ import { MdStarBorder, MdStarHalf, MdStarRate } from "react-icons/md";
 import PriceDetails from "./PriceDetails";
 import SpecsTable from "./SpecsTable";
 import StarRatings from "react-star-ratings";
+import { Link } from "react-router-dom";
 
 const ProductCard = props => {
 	const {
+		key,
 		title,
 		rating,
 		number_of_reviews,
@@ -22,88 +24,90 @@ const ProductCard = props => {
 	} = props;
 
 	return (
-		<Flex
-			flexDir={["column", "row"]}
-			shadow={"0 2px 2px #002A581a, 0 4px 4px #002A581a, 0 8px 8px #002A581a"}
-			border={"1px solid #e1e1e1"}
-			rounded={"base"}
-			p={3}
-		>
-			{/* product image */}
-			<Flex px={4} py={4} flex={1} align={"center"} justify={"center"}>
-				<Img
-					src={image_url[0]}
-					alt="product_image"
-					h={["130px", "auto"]}
-					objectFit={"contain"}
-				/>
-			</Flex>
-
-			{/* product details */}
-			<Flex flexDir={"column"} px={4} gap={1} flex={1.4}>
-				{/* title */}
-				<Text color={"brand"} fontSize="20px" lineHeight={"28px"}>
-					{title}
-				</Text>
-
-				{/* rating */}
-				<Flex align={"center"}>
-					<Flex fontSize={"24px"} color="brand" mr={2} align="center">
-						<StarRatings
-							rating={rating}
-							starRatedColor="#0076ce"
-							numberOfStars={5}
-							name="rating"
-							starDimension="20px"
-							starSpacing="0px"
-						/>
-					</Flex>
-					<Text fontSize={"0.9rem"}>{rating}&nbsp;</Text>
-					<Text fontSize={"0.9rem"}>({number_of_reviews})</Text>
+		<Link to={`/products/${key}`}>
+			<Flex
+				flexDir={["column", "row"]}
+				shadow={"0 2px 2px #002A581a, 0 4px 4px #002A581a, 0 8px 8px #002A581a"}
+				border={"1px solid #e1e1e1"}
+				rounded={"base"}
+				p={3}
+			>
+				{/* product image */}
+				<Flex px={4} py={4} flex={1} align={"center"} justify={"center"}>
+					<Img
+						src={image_url[0]}
+						alt="product_image"
+						h={["130px", "auto"]}
+						objectFit={"contain"}
+					/>
 				</Flex>
 
-				{/* small screen price details */}
-				<Box
-					display={["block", "block", "block", "block", "none"]}
-					m={"-12px"}
-					mt={1}
-				>
+				{/* product details */}
+				<Flex flexDir={"column"} px={4} gap={1} flex={1.4}>
+					{/* title */}
+					<Text color={"brand"} fontSize="20px" lineHeight={"28px"}>
+						{title}
+					</Text>
+
+					{/* rating */}
+					<Flex align={"center"}>
+						<Flex fontSize={"24px"} color="brand" mr={2} align="center">
+							<StarRatings
+								rating={rating}
+								starRatedColor="#0076ce"
+								numberOfStars={5}
+								name="rating"
+								starDimension="20px"
+								starSpacing="0px"
+							/>
+						</Flex>
+						<Text fontSize={"0.9rem"}>{rating}&nbsp;</Text>
+						<Text fontSize={"0.9rem"}>({number_of_reviews})</Text>
+					</Flex>
+
+					{/* small screen price details */}
+					<Box
+						display={["block", "block", "block", "block", "none"]}
+						m={"-12px"}
+						mt={1}
+					>
+						<PriceDetails
+							original_price={original_price}
+							discounted_price={discounted_price}
+						/>
+					</Box>
+
+					{/* specs */}
+					<Flex gap={2} flexDir="column">
+						<Heading
+							size={"md"}
+							borderBottom={"1px solid lightgray"}
+							pt={3}
+							pb={1}
+							mb={1}
+						>
+							Specs
+						</Heading>
+						<SpecsTable
+							processor={processor}
+							OS={OS}
+							graphics_card={graphics_card}
+							memory={memory}
+							storage={storage}
+							display={display}
+						/>
+					</Flex>
+				</Flex>
+
+				{/* price details */}
+				<Box display={["none", "none", "none", "none", "block"]}>
 					<PriceDetails
 						original_price={original_price}
 						discounted_price={discounted_price}
 					/>
 				</Box>
-
-				{/* specs */}
-				<Flex gap={2} flexDir="column">
-					<Heading
-						size={"md"}
-						borderBottom={"1px solid lightgray"}
-						pt={3}
-						pb={1}
-						mb={1}
-					>
-						Specs
-					</Heading>
-					<SpecsTable
-						processor={processor}
-						OS={OS}
-						graphics_card={graphics_card}
-						memory={memory}
-						storage={storage}
-						display={display}
-					/>
-				</Flex>
 			</Flex>
-
-			{/* price details */}
-			<Box display={["none", "none", "none", "none", "block"]}>
-				<PriceDetails
-					original_price={original_price}
-					discounted_price={discounted_price}
-				/>
-			</Box>
-		</Flex>
+		</Link>
 	);
 };
 
